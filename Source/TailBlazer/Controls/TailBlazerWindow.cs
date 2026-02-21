cab22c076e52447063adc3fe6dfa53f0f9a30873
EzraWard <52414057+EzraWard@users.noreply.github.com>
2026-02-20 19:39:21 -0500

    Improve settings page

--- Source/TailBlazer/Controls/TailBlazerWindow.cs
+++ Source/TailBlazer/Controls/TailBlazerWindow.cs
@@ -1,12 +1,34 @@
-﻿using System.Windows;
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace TailBlazer.Controls;
 
 public class TailBlazerWindow : Window
 {
+    private const int DwmWindowAttributeSystemBackdropType = 38;
+    private const int DwmWindowAttributeUseImmersiveDarkMode = 20;
+    private const int DwmSystemBackdropMainWindow = 2;
+
     static TailBlazerWindow()
     {
         DefaultStyleKeyProperty.OverrideMetadata(typeof(TailBlazerWindow), new FrameworkPropertyMetadata(typeof(TailBlazerWindow)));
+    }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+
+        if (PresentationSource.FromVisual(this) is not HwndSource source)
+        {
+            return;
+        }
+
+        var backdrop = DwmSystemBackdropMainWindow;
+        _ = DwmSetWindowAttribute(source.Handle, DwmWindowAttributeSystemBackdropType, ref backdrop, Marshal.SizeOf<int>());
+
+        var darkMode = 1;
+        _ = DwmSetWindowAttribute(source.Handle, DwmWindowAttributeUseImmersiveDarkMode, ref darkMode, Marshal.SizeOf<int>());
     }
 
     public static readonly DependencyProperty LeftHeaderContentProperty = DependencyProperty.Register(
@@ -34,4 +56,7 @@ public class TailBlazerWindow : Window
     {
         return (object)element.GetValue(RightHeaderContentProperty);
     }
+
+    [DllImport("dwmapi.dll")]
+    private static extern int DwmSetWindowAttribute(IntPtr hwnd, int dwAttribute, ref int pvAttribute, int cbAttribute);
 }
