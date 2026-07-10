d896a1a7fe7b589a2c2f8a42e9b40aba02c29149
EzraWard <52414057+EzraWard@users.noreply.github.com>
2026-07-10 15:32:27 -0400

    Better "file unavailable" view.

--- Source/Sawmill/Views/Tail/TailView.xaml.cs
+++ Source/Sawmill/Views/Tail/TailView.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media.Imaging;
 
 namespace Sawmill.Views.Tail;
 
@@ -18,21 +17,6 @@ public partial class TailView : UserControl
             FocusSearchTextBox();
         };
 
-        // Load inline icon image at runtime to avoid XAML parse errors if resource missing
-        try
-        {
-            var imgUri = new Uri("pack://application:,,,/Sawmill;component/Images/sawmill.png", UriKind.Absolute);
-            var bi = new BitmapImage();
-            bi.BeginInit();
-            bi.UriSource = imgUri;
-            bi.CacheOption = BitmapCacheOption.OnLoad;
-            bi.EndInit();
-            TailSawmillImage.Source = bi;
-        }
-        catch
-        {
-            // ignore
-        }
     }
 
     private void FocusSearchTextBox()
